9b5db3b14f2db0d57d57001d4ae422da980a5b1f
kenjiness <franciscoiiiavena@gmail.com>
2022-12-05 01:44:33 +0800

    hi e2 pala update ko bukas nalang ang next kase nakakapagod switches

--- avena_pacturan_java_project.java
+++ avena_pacturan_java_project.java
@@ -3,16 +3,29 @@
 import java.util.Scanner;
 public class avena_pacturan_java_project {
 
+    static void foodPriceCalculation()
+    {
+
+    }
+
+    static void drinkPriceCalculation()
+    {
+
+    }
 
     public static void main (String[]args)
     {
         Scanner input = new Scanner (System.in);
 
         int orderSelection;
+
         int foodOrderSelection;
+        int drinkOrderSelection;
+
+        int foodOrderNumber;
+        int drinkOrderNumber;
 
         
-
         System.out.println("Kan-anan ni Joross");
         System.out.println("Unsay order nimo doles? Press [1], [2], or [3]");
 
@@ -22,7 +35,7 @@ public class avena_pacturan_java_project {
         {
             case 1: //food
 
-                System.out.println("Please choose your order by entering [1-7]: ");
+                System.out.println("Please choose your order by entering [1-7] (Press [0] if you do not want any food.): ");
                 System.out.println("[1] Burger");
                 System.out.println("[2] Taco");
                 System.out.println("[3] Hotdog on Buns");
@@ -33,13 +46,34 @@ public class avena_pacturan_java_project {
 
                 foodOrderSelection = input.nextInt();
 
-            
+                switch (foodOrderSelection){
 
+                    case 0:
+                        break;
+                    case 1:
+                        System.out.println("How many orders?");
+                        foodOrderNumber = input.nextInt();
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
 
-                break;
+                }
+                
             
             case 2: //drinks
+                System.out.println("Please choose your order by entering [1-4] (Press [0] if you do not want any drinks.): ");
+                System.out.println("[1] Coke");
+                System.out.println("[2] Sprite");
+                System.out.println("[3] Royal");
+                System.out.println("[4] Mountain Dew");
+
                 break;
+
+
 
             case 3: //unsa may pwede na option diri
                 break;
