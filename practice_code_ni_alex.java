df88b9a7bd7316fecc708221d2adf9dc1fab8e7c
kenjiness <franciscoiiiavena@gmail.com>
2022-12-11 14:36:59 +0800

    Update practice_code_ni_alex.java

--- practice_code_ni_alex.java
+++ practice_code_ni_alex.java
@@ -11,37 +11,32 @@ public class practice_code_ni_alex {
     
     System.out.println("Please input your values now"); //asks for user input
  
-    while(ar)
+    while(ar) // while ar remains true, do
     {
         if(input.hasNextInt()) //if input is int
         {
-            arn = input.nextInt();
-            String gbc = String.valueOf(arn); //convert input into string
-            abc += " ";
-            abc += gbc;
+            arn = input.nextInt(); // stores data input as int
+            String gbc = String.valueOf(arn); // convert int input into string
+            abc += " "; // adds " " to abc = ""
+            abc += gbc; // adds the string in String gbc into abc
         }
         else
         {
-            ar=false;
+            ar=false; // if data input != int, then end loop
         }
     }
  
-        String[] alt = abc.split(" "); //
+        String[] alt = abc.split(" "); // splits the array contents by using " " as separator
+
         String[] prox = new String[alt.length - 1];
  
- 
-    for (int i = 0;i <= prox.length-1; i++)
- 
-    {
- 
-    prox[i]=alt[i+1];
- 
-    }
- 
+    for (int i = 0; i <= prox.length-1; i++)
+        {
+            prox [i] = alt [i + 1];
+        }
+
     System.out.println("Output: " + Arrays.toString(prox));
- 
     loop=false;
- 
     System.exit(0);
  
  
