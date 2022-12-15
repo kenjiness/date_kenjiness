232a3e890bda4bfde8b421444aaf9794c6d2d9e9
kenjiness <franciscoiiiavena@gmail.com>
2022-12-16 00:20:42 +0800

    new project: inventory system

---
+++ InventorySystem/MainInventorySystem.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class MainInventorySystem {
+    public static void main (String [] args)
+    {
+        Scanner input = new Scanner (System.in);
+
+        String itemName, itemType, itemSize;
+        long itemID;
+        int proceed, itemQuantity, itemPrice;
+
+        do
+        {
+        System.out.println("Welcome to insertInventoryNameHere! ");
+        System.out.println("Press [1] to proceed: ");
+        proceed = input.nextInt();
+        }
+        while (proceed != 1)
+
+        
+    }
+}
